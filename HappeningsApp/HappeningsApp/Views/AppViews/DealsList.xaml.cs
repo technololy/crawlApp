4420e55a75547dbe4e58de3e69a047955f82ae32
Ololade Oyebanji <loladeking@gmail.com>
2018-09-08 23:52:47 +0100

    changes on ios

---
+++ HappeningsApp/HappeningsApp/Views/AppViews/DealsList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HappeningsApp.Views.AppViews
+{
+    public partial class DealsList : ContentPage
+    {
+        public DealsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
