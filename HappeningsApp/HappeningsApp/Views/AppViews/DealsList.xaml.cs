86ff31a0d41909d6e6cee01884a19db50499895c
Ololade Oyebanji <loladeking@gmail.com>
2018-09-09 00:02:58 +0100

    new

--- HappeningsApp/HappeningsApp/Views/AppViews/DealsList.xaml.cs
+++ HappeningsApp/HappeningsApp/Views/AppViews/DealsList.xaml.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using HappeningsApp.ViewModels;
 using Xamarin.Forms;
 
 namespace HappeningsApp.Views.AppViews
 {
     public partial class DealsList : ContentPage
     {
+        IntroPageViewModel introPageViewMod;
+
         public DealsList()
         {
             InitializeComponent();
+            introPageViewMod = new IntroPageViewModel();
+            //nearBy = new NearByViewModel();
+            BindingContext = introPageViewMod;
+
         }
     }
 }
