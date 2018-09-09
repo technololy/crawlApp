df17a85c47ec7d47af7f6ae16984f5a67e5df39e
Ololade Oyebanji <loladeking@gmail.com>
2018-09-09 22:28:38 +0100

    changes

---
+++ HappeningsApp/HappeningsApp/Views/LoggedOn.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HappeningsApp.Views
+{
+    public partial class LoggedOn : ContentPage
+    {
+        public LoggedOn()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HappeningsApp/HappeningsApp/Views/facebooktestpage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HappeningsApp.Views
+{
+    public partial class facebooktestpage : ContentPage
+    {
+        public facebooktestpage()
+        {
+            InitializeComponent();
+        }
+    }
+}
