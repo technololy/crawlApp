2a337ea027771240605c03b3921f4de97c7cf397
loladeking <loladeking@gmail.com>
2018-10-05 00:33:22 +0100

    more o viewmodel

--- HappeningsApp/HappeningsApp/Custom/MMultiSelectPopUpViewModel.cs
+++ HappeningsApp/HappeningsApp/Custom/MMultiSelectPopUpViewModel.cs
@@ -8,12 +8,22 @@ namespace HappeningsApp.Custom
 {
     public class MMultiSelectPopUpViewModel : INotifyPropertyChanged
     {
+        private bool _selectedSwitch;
+
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        public bool SelectedSwitch { get; set; }
+        public bool SelectedSwitch
+        {
+            get => _selectedSwitch;
+            set
+            {
+                _selectedSwitch = value;
+                OnPropertyChanged();
+            }
+        }
 
     }
 }
