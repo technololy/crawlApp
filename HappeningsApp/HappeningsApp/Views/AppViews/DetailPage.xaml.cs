2295d6d28c8a2a85c8209abda5e3c7899d121cec
Ololade Oyebanji <loladeking@gmail.com>
2018-12-09 19:18:06 +0100

    no message

--- HappeningsApp/HappeningsApp/Views/AppViews/DetailPage.xaml.cs
+++ HappeningsApp/HappeningsApp/Views/AppViews/DetailPage.xaml.cs
@@ -129,31 +129,7 @@ namespace HappeningsApp.Views.AppViews
 
 
                 }
-                //var uberLaunch = $"https://m.uber.com/ul/?action=setPickup&pickup[nickname]={encodedAddress}&" +
-                //	"pickup[formatted_address]=my_location&dropoff[nickname]={encodedAddress}&dropoff" +
-                //	"[formatted_address]={encodedAddress}";
 
-                //var uberLaunch2 = "https://m.uber.com/ul/?action=setPickup&pickup[nickname]=lagos&" +
-                //	"pickup[formatted_address]=my_location&dropoff[latitude]=37.802374&dropoff[longitude]" +
-                //	"=-122.405818&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=" +
-                //	"1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133";
-                ////Device.OpenUri(new Uri("uber://?addr=" + MapsAddressPlus));
-
-                //var uberLaunch3 = $"https://m.uber.com/ul/?action=setPickup&pickup[latitude]=37.775818&pickup" +
-                //	"[longitude]=-122.418028&pickup[nickname]=UberHQ&pickup[formatted_address]=" +
-                //	"1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=" +
-                //	"37.802374&dropoff[longitude]=-122.405818&dropoff[nickname]=" +
-                //	"Coit%20Tower&dropoff[formatted_address]=" +
-                //	"1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=" +
-                //	"a1111c8c-c720-46c3-8534-2fcdd730040d&link_text=View%20team%20roster&partner_deeplink" +
-                //	"=partner%3A%2F%2Fteam%2F9383\n";
-
-                //var uberLaunch33 = "https://m.uber.com/ul/?action=setPickup&pickup=my_location&dropoff[latitude]=" +
-                //  "6.448936&dropoff[longitude]=3.3936575&dropoff[nickname]="+encodedAddress+"&dropoff" +
-                //  "[formatted_address]="+encodedAddress+"&link_text=View%20team%20roster&partner_deeplink" +
-                //  "=partner%3A%2F%2Fteam%2F9383\n";
-
-                //Device.OpenUri(new Uri(uberLaunch33));
             }
             catch (Exception ex)
             {
